cb63189e67bc7fefdd61d9f17276f52264bc6cba
sabrisilvahbr-debug <sabrisilvahbr@gmail.com>
2026-04-15 13:41:42 -0300

    feat: add src/pages/HomePage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import Hero from '../components/Hero'
+import Features from '../components/Features'
+import CTA from '../components/CTA'
+
+
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      
+      <Features />
+      <CTA />
+    </>
+  )
+}
